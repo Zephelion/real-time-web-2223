47b6dcfee707f996f851e313d17acb9466dd5ca2
Zephelion <russellnumo@outlook.com>
2023-04-21 16:10:22 +0200

    introduction page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { introduction } from '../controllers/UserController.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index')
 });
+
+router.get('/introduction', introduction);
 
 
 
