218907482a4bf9edefdf0b9a799325c3fbd7649a
Zephelion <russellnumo@outlook.com>
2023-05-01 19:12:13 +0200

    :trollface: misc

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ app.use(session({
 app.engine('hbs', engine({extname: 'hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', './views');
-// app.set('io', io);
 app.use(express.static('public'));
 
 
