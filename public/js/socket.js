33cefb1a6b34b4ac6a874bcb55e138e3c1ec55de
Zephelion <russellnumo@outlook.com>
2023-05-12 00:15:51 +0200

    console errors

--- public/js/socket.js
+++ public/js/socket.js
@@ -29,121 +29,82 @@ script.async = true;
 document.body.appendChild(script);
 
 window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = lobbySection.dataset.token;
+    if(lobbySection){
+        const token = lobbySection.dataset.token;
 
-    console.log(token);
 
-    const player = new Spotify.Player({
-        name: 'Web Playback SDK Quick Start Player',
-        getOAuthToken: cb => { cb(token); }
-        
-    });
+        console.log(token);
 
-    player.addListener('ready', async ({ device_id }) => {
-        console.log('Ready with Device ID', device_id);
-        await fetch(`https://api.spotify.com/v1/me/player`, {
-            method: 'PUT',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify({
-                device_ids: [device_id],
-                play: false,
-            }),
+        const player = new Spotify.Player({
+            name: 'Web Playback SDK Quick Start Player',
+            getOAuthToken: cb => { cb(token); }
+            
         });
-
-        const response = await fetch (`/getcurrentsong/${roomId}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-        })
-        const data = await response.json();
-        currentSong = data.currentSong;
-
-        console.log(currentSong);
-
-        await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify({
-                uris: [currentSong],
-                play: false,
-            }),
-        });
-    });
-
-    // Error handling
-    player.addListener('not_ready', ({ device_id }) => {
-        console.log('Device ID has gone offline', device_id);
-    });
-
-    player.addListener('initialization_error', ({ message }) => {
-        console.error(message);
-    });
-  
-    player.addListener('authentication_error', ({ message }) => {
-        console.error(message);
-    });
-  
-    player.addListener('account_error', ({ message }) => {
-        console.error(message);
-    });
-
-
-
-
-    socket.on('play', (roomId, uri) => {
-        player.togglePlay();
-        player.getCurrentState().then(async state => {
-            console.log(state);
-            currentSong = state.track_window.current_track.uri;
+    
+        player.addListener('ready', async ({ device_id }) => {
+            console.log('Ready with Device ID', device_id);
+            await fetch(`https://api.spotify.com/v1/me/player`, {
+                method: 'PUT',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify({
+                    device_ids: [device_id],
+                    play: false,
+                }),
+            });
+    
+            const response = await fetch (`/getcurrentsong/${roomId}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            const data = await response.json();
+            currentSong = data.currentSong;
+    
             console.log(currentSong);
-            await fetch(`/currentsong/${currentSong}/${roomId}` , {
+    
+            await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
                     Authorization: `Bearer ${token}`,
                 },
                 body: JSON.stringify({
-                    currentSong,
+                    uris: [currentSong],
+                    play: false,
                 }),
-            })
-
+            });
         });
-    });
-
-    socket.on('message', (name, roomId, currentSong) => {
-        console.log('Song currently playing: ', currentSong);
-        const li = document.createElement('li');
-        li.textContent = name;
-        participants.appendChild(li);
-        console.log(`${name} joined room ${roomId}`);
-    });
-
-    player.addListener('player_state_changed', (state) => {
-        if(!state) {
-            return;
-        }
-        // const { uri } = state.track_window.current_track;
-        // socket.emit('play', roomId, uri);
-    })
-
-        
-    playBtn.addEventListener('click', async (e) => {
-        socket.emit('play', roomId, 'spotify:track:2yr2HnFYl7XvqJk4fXoQBt');
-        // player.togglePlay();
-    });
-
-    nextBtn.addEventListener('click', (e) => {
-        player.nextTrack().then(() => {
+    
+        // Error handling
+        player.addListener('not_ready', ({ device_id }) => {
+            console.log('Device ID has gone offline', device_id);
+        });
+    
+        player.addListener('initialization_error', ({ message }) => {
+            console.error(message);
+        });
+      
+        player.addListener('authentication_error', ({ message }) => {
+            console.error(message);
+        });
+      
+        player.addListener('account_error', ({ message }) => {
+            console.error(message);
+        });
+    
+    
+    
+    
+        socket.on('play', (roomId, uri) => {
+            player.togglePlay();
             player.getCurrentState().then(async state => {
-                console.log(currentSong);
+                console.log(state);
                 currentSong = state.track_window.current_track.uri;
+                console.log(currentSong);
                 await fetch(`/currentsong/${currentSong}/${roomId}` , {
                     method: 'PUT',
                     headers: {
@@ -154,22 +115,66 @@ window.onSpotifyWebPlaybackSDKReady = () => {
                         currentSong,
                     }),
                 })
+    
             });
         });
-    });
-
-    previousBtn.addEventListener('click', (e) => {
-        player.previousTrack();
-    });
-
-    player.connect();
+    
+        socket.on('message', (name, roomId, currentSong) => {
+            console.log('Song currently playing: ', currentSong);
+            const li = document.createElement('li');
+            li.textContent = name;
+            participants.appendChild(li);
+            console.log(`${name} joined room ${roomId}`);
+        });
+    
+        player.addListener('player_state_changed', (state) => {
+            if(!state) {
+                return;
+            }
+            // const { uri } = state.track_window.current_track;
+            // socket.emit('play', roomId, uri);
+        })
+    
+            
+        playBtn.addEventListener('click', async (e) => {
+            socket.emit('play', roomId, 'spotify:track:2yr2HnFYl7XvqJk4fXoQBt');
+            // player.togglePlay();
+        });
+    
+        nextBtn.addEventListener('click', (e) => {
+            player.nextTrack().then(() => {
+                player.getCurrentState().then(async state => {
+                    console.log(currentSong);
+                    currentSong = state.track_window.current_track.uri;
+                    await fetch(`/currentsong/${currentSong}/${roomId}` , {
+                        method: 'PUT',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            Authorization: `Bearer ${token}`,
+                        },
+                        body: JSON.stringify({
+                            currentSong,
+                        }),
+                    })
+                });
+            });
+        });
+    
+        previousBtn.addEventListener('click', (e) => {
+            player.previousTrack();
+        });
+    
+        player.connect();
+    }
 
 };
 
 
-leaveRoomBtn.addEventListener('click', () => {
-    socket.emit('leaveRoom', roomId, localStorage.getItem('name'));
-});
+if(leaveRoomBtn) {
+    leaveRoomBtn.addEventListener('click', () => {
+        socket.emit('leaveRoom', roomId, localStorage.getItem('name'));
+    });
+}
 
 socket.on('leave-room', (name, roomId) => {
     console.log(`${name} left room ${roomId}`);
