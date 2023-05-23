033f4365af176a60807871c01bff4518a1eb80a1
Zephelion <russellnumo@outlook.com>
2023-05-23 16:28:06 +0200

    changed callback link

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -16,7 +16,7 @@ export const introduction = (req, res) => {
 export const spotifyApi = new SpotifyWebApi({
     clientId: clientId,
     clientSecret: clientSecret,
-    redirectUri: 'http://localhost:3000/callback'
+    redirectUri: 'https://real-time-web-2223-production-0bfa.up.railway.app/callback'
 });
 
 
